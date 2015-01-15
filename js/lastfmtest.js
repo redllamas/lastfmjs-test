d49eb9b92c6d13f972e5c527cae021862b188cdb
redllamas <sam@jetsets.jp>
2015-01-15 12:40:15 +0900

    removed native Javascript to focus on jQuery. This way we can get a username from the user input

--- js/lastfmtest.js
+++ js/lastfmtest.js
@@ -1,56 +1,34 @@
 /* 
-  This will be the base for a test app called lastfm-test, which will output
-  info from last.fm for my account
+  redllamas 2015
 */
 
 var params = {
   url: "http://ws.audioscrobbler.com/2.0/",
   key: "0eabfc4a8115137c43388f0e23ab6afe",
-  user: "etrinsou"
+  user: ""
 };
 
-$(document).ready(function() {  
-    $("#getViajQ").click(function() { 
-      $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists" + "&user="+ params.user + "&api_key=" + params.key + "&limit=15&format=json&callback=?", function(json) {  
-        var html = '',
-            phtml = '';  
-        $.each(json.topartists.artist, function(i, item) {  
-            html += "<p><a href=" + item.url + " target='_blank'>" + item.name + " - " + "Play count : " + item.playcount + "</a></p>";
-            phtml +=  "<a href=" + item.url + " target='_blank'><img src=" + item.image[1]["#text"] + " alt=" + item.name + " /></a>";
-        });  
-        $('#outbox').append(html);
-        $('#photobox').append(phtml);  
-      });
+$(document).ready(function() {
+      // by clicking on the "Let's go!" button, we trigger the event
+      $("#getData").click(function() {
+          // first make sure the output boxes are empty
+          $('#outbox').empty();
+          $('#photobox').empty();
+          // we set the username by obtaining the value from the input box
+          params.user = $("#lfminfo").val();
+          // then we get the JSON, using params and the username we just got
+          $.getJSON(params.url + "?method=user.getTopArtists&user="+ params.user + "&api_key=" + params.key + "&limit=15&format=json&callback=?", function(json) {  
+            // define the content to go in output
+            var html = '',
+                phtml = '';
+            // parse each object in the array
+            $.each(json.topartists.artist, function(i, item) {  
+                html += "<p><a href=" + item.url + " target='_blank'>" + item.name + " - " + "Play count : " + item.playcount + "</a></p>";
+                phtml += "<a href=" + item.url + " target='_blank'><img src=" + item.image[1]["#text"] + " alt=" + item.name + " /></a>";
+            });
+            // then output the HTML to the DOM
+            $('#outbox').append(html);
+            $('#photobox').append(phtml);
+        });
   });
 });
-
-
-var jsurl = params.url;
-jsurl += "?method=user.getTopArtists";
-jsurl += "&user="+ params.user;
-jsurl += "&api_key=" + params.key;
-jsurl += "&limit=15&format=json"; // &callback=jscall
-
-function reqListener () {
-    console.log(this.responseText);
-}
-
-var oReq = new XMLHttpRequest();
-oReq.onload = reqListener;
-oReq.open("get", jsurl, true);
-oReq.send();
-
-function outputtopartists() {
-  var output = JSON.parse(oReq.response);
-  var html = '';
-  var outbox = document.getElementById("outbox");
-  var phtml = '';
-  var photobox = document.getElementById("photobox");
-  for (var i = 0; i < output.topartists.artist.length; i++){
-   html += "<p><a href=" + output.topartists.artist[i].url + " target='_blank'>" + output.topartists.artist[i].name + " - " + "Play count : " + output.topartists.artist[i].playcount + "</a></p>";
-   outbox.innerHTML = html;
-   phtml += "<a href=" + output.topartists.artist[i].url + " target='_blank'>" + "<img src=" + output.topartists.artist[i].image[1]["#text"] + " alt=" + output.topartists.artist[i].name + " /></a>";
-   photobox.innerHTML = phtml;
-  }
-
-}
